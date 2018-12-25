c8489e001b66934b9f367b3d02a3dd1c137c24bc
Cheng Vue <cvu3@ucsc.edu>
2018-12-25 22:07:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,8 +38,8 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
-  def self.create(name, grade)
-    student = Student.new(name, grade)
+  def self.create(student)
+    student = Student.new(student.name, student.grade)
     student.save
     student
   end
