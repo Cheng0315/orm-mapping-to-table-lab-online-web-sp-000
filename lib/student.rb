d42055a2cdc0f9878d6c66d6a3e4baaced503c63
Cheng Vue <cvu3@ucsc.edu>
2018-12-25 21:33:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,7 @@ class Student
 
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE if NOT EXISTS students (
+      CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
       grade INTEGER
