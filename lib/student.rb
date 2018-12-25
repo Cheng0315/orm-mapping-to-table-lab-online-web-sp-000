d9d6150f04f9a3c46592d7135bca2cc408f3590d
Cheng Vue <cvu3@ucsc.edu>
2018-12-25 21:29:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
       CREATE TABLE if NOT EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
-      grade = INTEGER
+      grade INTEGER
     )
     SQL
     DB[:conn].execute(sql)
