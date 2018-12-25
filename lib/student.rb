7f61ea163c5ebc6c64dd45c3398945c126ec44a1
Cheng Vue <cvu3@ucsc.edu>
2018-12-25 21:27:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,14 +1,15 @@
 class Student
   attr_accessor :name, :grade
   attr_reader :id
-  
+
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE if NOT EXISTS students 
+      CREATE TABLE if NOT EXISTS students
       id INTEGER PRIMARY KEY,
       name TEXT,
       grade = INTEGER
     SQL
+    DB[:conn].execute(sql)
   end
 
 end
